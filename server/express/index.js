a969cbc5360eb4cd3e4b8147151445c9f56a3933
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-04-21 12:30:47 +0100

    intial deploy

--- server/express/index.js
+++ server/express/index.js
@@ -3,8 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const router = require('./router');
 const cors = require('cors');
+const path = require('path');
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,6 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../next/.next'));
 
+  app.get('*', (req, res) => {
+  //  res.sendFile(path.join(__dirname, 'next', '.next', )); 
+  });
+}
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`)); //eslint-disable-line
+app.listen(port, () => console.log(`Server listening at ${port}`)); //eslint-disable-line
