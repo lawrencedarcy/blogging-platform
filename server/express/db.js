23b7622df694c6ada639fdc6c3e4f5165785cfec
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-04-21 13:44:49 +0100

    redeploy to heroku"

--- server/express/db.js
+++ server/express/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const connectionString = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@stagetime-bs3vr.mongodb.net/test?retryWrites=true&w=majority`;
 
+console.log('dbname', process.env.DB_NAME);
 mongoose.connect(connectionString, { useNewUrlParser: true }, err => {
   if (err) console.log(err); // eslint-disable-line
   else console.log('mongoose connected!'); // eslint-disable-line
